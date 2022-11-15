c562423db6cd8eecf3b9275a6e9ac8d955cee86d
aanamshaikh <aanamshaikh01@gmail.com>
2022-11-15 09:46:22 +0530

    wrote test cases

--- student-grading/student.go
+++ student-grading/student.go
@@ -8,6 +8,8 @@ import (
 	"log"
 	"os"
 	"sort"
+
+	// "sort"
 	"strconv"
 )
 
@@ -19,12 +21,15 @@ type student struct {
 	test2Score float64
 	test3Score float64
 	test4Score float64
-
-	// finalScore float64
-	// grade string
 }
 
-func (student student) getAverageScore() float64 {
+type studentGrade struct {
+	student    student
+	finalScore float64
+	grade      string
+}
+
+func getAverageScore(student student) float64 {
 	average := (student.test1Score + student.test2Score + student.test3Score + student.test4Score) / 4
 	return float64(average)
 }
@@ -32,7 +37,6 @@ func (student student) getAverageScore() float64 {
 func calculateGrade(average float64) string {
 
 	if average > 90 {
-
 		return "A"
 	} else if average > 80 && average < 90 {
 		return "B"
@@ -46,36 +50,24 @@ func calculateGrade(average float64) string {
 
 }
 
-func overallTopper(students []student) student {
-	topper := student{"", "", "", 0, 0, 0, 0}
-	var highestScore float64
-
-	for i := 0; i < len(students); i++ {
-
-		if students[i].getAverageScore() > highestScore {
-			topper = students[i]
-		}
-
-	}
-	return topper
-}
-
-func ParseToFloat(number string) float64 {
-	num, _ := strconv.ParseInt(number, 10, 64)
-	// fmt.Println(number,num)
-	return float64(num)
-}
-
 func parseCSV(filepath string) []student {
-	csvFile, _ := os.Open(filepath)
+	csvFile, err := os.Open(filepath)
+	if err != nil {
+		log.Println(err)
+		return make([]student, 0)
+	}
+	defer csvFile.Close()
 	reader := csv.NewReader(bufio.NewReader(csvFile))
+
 	var students []student
 	for {
-		line, error := reader.Read()
-		if error == io.EOF {
+		line, err := reader.Read()
+		// refactor
+		if err == io.EOF {
 			break
-		} else if error != nil {
-			log.Fatal(error)
+		}
+		if err != nil {
+			log.Println(err)
 		}
 
 		students = append(students, student{
@@ -91,43 +83,65 @@ func parseCSV(filepath string) []student {
 	return students
 }
 
-func getScoreAndGradeForStudent(students []student) {
-	for _, students := range students {
-		finalScore := students.getAverageScore()
+func getScoreAndGrades(students []student) []studentGrade {
+
+	studentGrades := make([]studentGrade, 0)
+	for _, s := range students {
+		finalScore := getAverageScore(s)
 		grade := calculateGrade(finalScore)
-		fmt.Printf("The final Score of Student %v %v is %v and the grade is %v\n", students.firstname, students.lastname, finalScore, grade)
+		sg := studentGrade{student: s, finalScore: finalScore, grade: grade}
+		studentGrades = append(studentGrades, sg)
+
 	}
+	return studentGrades
 }
 
-func topperPerUniversity(students []student) map[string]student {
-	studentList := make(map[string]student)
-	listOfStudents:=students
-	 sort.Slice(listOfStudents, func(i, j int) bool {
-		return listOfStudents[i].university < listOfStudents[j].university
+func overallTopper(students []studentGrade) studentGrade {
+	topper := students
+	sort.Slice(topper, func(i, j int) bool {
+		return topper[i].finalScore < topper[j].finalScore
 	})
-	
-	i:=0
-	lastIndexUniv:=0
+	return topper[len(topper)-1]
+}
 
-	for i=0;i<len(listOfStudents);i++{ 
-			if listOfStudents[i].university!=listOfStudents[lastIndexUniv].university {
-				lastIndexUniv=i
+func ParseToFloat(number string) float64 {
+	num, _ := strconv.ParseInt(number, 10, 64)
+	// fmt.Println(number,num)
+	return float64(num)
+}
+
+func topperPerUniversity(students []studentGrade) map[string]studentGrade {
+	topperPerUniversity := make(map[string]studentGrade, 0) // mum
+
+	for _, s := range students { // Mum ,pune, delhi
+
+		student, ok := topperPerUniversity[s.student.university] //mumbai
+		if ok {
+			if s.finalScore > student.finalScore {
+				topperPerUniversity[s.student.university] = s
 			}
-			studentList[listOfStudents[i].university]=overallTopper(listOfStudents[lastIndexUniv:i])
+		} else {
+			topperPerUniversity[s.student.university] = studentGrade{student: s.student, finalScore: s.finalScore, grade: s.grade}
+		}
 	}
-	studentList[listOfStudents[lastIndexUniv].university]=overallTopper(listOfStudents[lastIndexUniv:])
-	
-	return studentList
+	return topperPerUniversity
 }
 
 func main() {
 	var studentsList []student = parseCSV("student-data.csv")
 
-	getScoreAndGradeForStudent(studentsList)
+	studentGrades := getScoreAndGrades(studentsList)
+	for _, s := range studentGrades {
 
-	topper := overallTopper(studentsList)
-	fmt.Printf("The overall Topper is %v %v with the score %v", topper.firstname, topper.lastname, topper.getAverageScore())
-   
-	topperPerUniversity(studentsList)
-	fmt.Println("Students: ",topperPerUniversity(studentsList))
+		fmt.Printf("student %v %v has secured %v marks with %v grade\n", s.student.firstname, s.student.lastname, s.finalScore, s.grade)
+	}
+
+	topper := overallTopper(studentGrades)
+	fmt.Printf("The overall Topper is %v %v with the score %v \n", topper.student.firstname, topper.student.lastname, topper.finalScore)
+
+	students := topperPerUniversity(studentGrades)
+	for _, s := range students {
+		fmt.Printf("The topper for %v is %v %v with the score %v\n", s.student.university, s.student.firstname, s.student.lastname, s.finalScore)
+	}
+
 }
---
+++ student-grading/student_test.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCalculateScoreAndGrade(t *testing.T) {
+	stud := student{"Aanam", "Shaikh", "Mumbai University", 78, 59, 69, 59}
+	actualString := getAverageScore(stud)
+	expectedString := 66.25
+	if actualString != expectedString {
+		t.Errorf("Expected String(%v) is not same as"+
+			" actual string (%v)", expectedString, actualString)
+	}
+}
+
+func TestCalculateGrade(t *testing.T) {
+	stud := student{"Aanam", "Shaikh", "Mumbai University", 78, 59, 69, 59}
+	avg := getAverageScore(stud)
+	actualString := calculateGrade(avg)
+	expectedString := "C"
+	if actualString != expectedString {
+		t.Errorf("Expected String(%v) is not same as"+
+			" actual string (%v)", expectedString, actualString)
+	}
+}
+
+func TestGetOverallTopper(t *testing.T) {
+
+	students := make([]studentGrade, 0)
+	students = append(students, studentGrade{student{"Aanam", "Shaikh", "Mumbai University", 99, 99, 99, 99}, 99.0, "A"})
+	students = append(students, studentGrade{student{"Aanam", "Shaikh", "Mumbai University", 78, 59, 69, 59}, 0, "F"})
+	students = append(students, studentGrade{student{"Aanam", "Shaikh", "Mumbai University", 78, 59, 69, 59}, 0, "F"})
+	students = append(students, studentGrade{student{"Aanam", "Shaikh", "Mumbai University", 78, 59, 69, 59}, 0, "F"})
+
+	topper := overallTopper(students)
+	expectedTopper := studentGrade{student{"Aanam", "Shaikh", "Mumbai University", 99, 99, 99, 99}, 99.0, "A"}
+
+	if topper != expectedTopper {
+		t.Errorf("The expected topper is: %v, but got %v", expectedTopper, topper)
+	}
+}
+
+func TestUniversityTopper(t *testing.T) {
+
+	students := make([]studentGrade, 0)
+	students = append(students, studentGrade{student{"Aanam", "Shaikh", "Mumbai University", 99, 99, 99, 99}, 99.0, "A"})
+	students = append(students, studentGrade{student{"Aanam", "Shaikh", "Mumbai University", 78, 59, 69, 59}, 66.25, "C"})
+	students = append(students, studentGrade{student{"Aanam", "Shaikh", "Pune University", 78, 59, 69, 59}, 66.25, "C"})
+	students = append(students, studentGrade{student{"Aanam", "Shaikh", "Delhi University", 78, 59, 69, 59}, 66.25, "C"})
+
+	topperStudent := make(map[string]studentGrade)
+	topperStudent["Mumbai University"] = studentGrade{student{"Aanam", "Shaikh", "Mumbai University", 99, 99, 99, 99}, 99.0, "A"}
+	topperStudent["Pune University"] = studentGrade{student{"Aanam", "Shaikh", "Pune University", 78, 59, 69, 59}, 66.25, "C"}
+	topperStudent["Delhi University"] = studentGrade{student{"Aanam", "Shaikh", "Delhi University", 78, 59, 69, 59}, 66.25, "C"}
+
+	topperPerUniversity := topperPerUniversity(students)
+
+	if !reflect.DeepEqual(topperStudent, topperPerUniversity) {
+		t.Errorf("Expected value %v, but got %v", topperStudent, topperPerUniversity)
+	}
+}
+
+func TestParseCSV(t *testing.T) {
+	actualList := make([]student, 0)
+	actualList = append(actualList, student{"Aanam", "Shaikh", "Mumbai University", 45, 56, 78, 30})
+	actualList = append(actualList, student{"Tom", "Fellon", "Pune University", 89, 56, 78, 34})
+	actualList = append(actualList, student{"jane", "Doe", "Delhi University", 55, 96, 90, 37})
+	actualList = append(actualList, student{"Tom", "smith", "Mumbai University", 59, 96, 78, 37})
+	actualList = append(actualList, student{"John", "Doe", "Delhi University",55, 96, 98, 37})
+	actualList = append(actualList, student{"John", "smith", "Delhi University",55, 76, 99, 97})
+
+	expectedList := parseCSV("student-data.csv")
+	
+    if actualList[0]!=expectedList[0] {
+        t.Errorf("Expected  Student(%v) is not same as"+
+			" actual Student (%v)", expectedList[0], actualList[0])
+    }
+
+    if actualList[len(actualList)-1]!=expectedList[len(expectedList)-1] {
+        t.Errorf("Expected Student(%v) is not same as"+
+			" actual Student (%v)",expectedList[len(expectedList)-1],actualList[len(actualList)-1])
+    }
+
+    if len(actualList)!=len(expectedList){
+        t.Errorf("Expected size(%v) is not same as"+
+			" actual size (%v)",len(expectedList) ,len(expectedList))
+    }
+}
